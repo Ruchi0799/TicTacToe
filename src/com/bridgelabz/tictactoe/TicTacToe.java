69d54785c186d0f9ca201a35cd483dae313b67b3
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-28 11:48:30 +0530

    [ruchi]: implemented allow player to select a letter and determine computer letter to play game

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void initializeBoard(){
         char[] boardArray=new char[10];
@@ -7,6 +9,21 @@ public class TicTacToe {
         {
             boardArray[i]=' ';
         }
+    }
+
+    public static void userInput(){
+        char computerLetter;
+        System.out.println("To play choose letter O or X");
+        Scanner sc=new Scanner(System.in);
+        char playerLetter=sc.next().charAt(0);
+        if(playerLetter=='O'){
+            computerLetter='X';
+        }
+        else {
+            computerLetter='O';
+        }
+        System.out.println("User letter: "+playerLetter);
+        System.out.println("Computer letter: "+computerLetter);
     }
 
 }
--- src/com/bridgelabz/tictactoe/TicTacToeRunner.java
+++ src/com/bridgelabz/tictactoe/TicTacToeRunner.java
@@ -4,5 +4,6 @@ public class TicTacToeRunner {
     public static void main(String[] args) {
         TicTacToe game=new TicTacToe();
         TicTacToe.initializeBoard();
+        TicTacToe.userInput();
     }
 }
