33b54567f847b3380d208e37fc7347a769929ee5
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-28 11:37:24 +0530

    [ruchi]: implemented intialize char Tictactoe board with 0

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -3,9 +3,9 @@ package com.bridgelabz.tictactoe;
 public class TicTacToe {
     public static void initializeBoard(){
         char[] boardArray=new char[10];
-        for (int i=1;i<10;i++)
+        for (int i=1;i< boardArray.length;i++)
         {
-            boardArray[i]='0';
+            boardArray[i]=' ';
         }
     }
 
